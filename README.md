55f7611090008777fa3b2b7154d54b7900df7482
Boshen <boshenc@gmail.com>
2026-07-15 20:26:44 +0800

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# [Sponsored By](https://oxc.rs/sponsor)
+
+<p align="center">
+  <a href="https://oxc.rs/sponsor">
+    <img src="https://raw.githubusercontent.com/oxc-project/sponsors/main/sponsors.svg" alt="Our sponsors" />
+  </a>
+</p>
---
+++ sponsorkit.config.js
@@ -0,0 +1,39 @@
+import { defineConfig, tierPresets } from 'sponsorkit'
+
+export default defineConfig({
+  opencollective: {
+    slug: "oxc",
+    type: 'collective'
+  },
+  outputDir: '.',
+  width: 800,
+  formats: ['svg', 'png', 'webp', 'json'],
+  renderer: 'tiers',
+  prorateOnetime: true,
+  tiers: [
+    {
+      title: 'Backers',
+      preset: tierPresets.small,
+    },
+    {
+      title: 'Supporters',
+      monthlyDollars: 20,
+      preset: tierPresets.base,
+    },
+    {
+      title: 'Bronze Sponsors',
+      monthlyDollars: 100,
+      preset: tierPresets.medium,
+    },
+    {
+      title: 'Silver Sponsors',
+      monthlyDollars: 200,
+      preset: tierPresets.large,
+    },
+    {
+      title: 'Gold Sponsors',
+      monthlyDollars: 500,
+      preset: tierPresets.xl,
+    },
+  ],
+})
